a01612453f18c2883f24e38a548ed2276616f69c
Emmanuel Franco <emmanuelfranco2001@gmail.com>
2024-06-18 18:13:04 -0300

    controller

--- src/main/java/me/dio/domain/repository/UserRepository.java
+++ src/main/java/me/dio/domain/repository/UserRepository.java
@@ -8,4 +8,7 @@ import me.dio.domain.model.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
+    boolean existsByAccountNumber(String number);
+
+    boolean existsByCardNumber(String number);
 }
