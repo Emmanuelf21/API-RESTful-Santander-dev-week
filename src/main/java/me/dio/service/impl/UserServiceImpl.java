96bd0dc99018fb468ee7492a8fe507c8a87375a2
Emmanuel Franco <emmanuelfranco2001@gmail.com>
2024-06-18 18:18:13 -0300

    impl

--- src/main/java/me/dio/service/impl/UserServiceImpl.java
+++ src/main/java/me/dio/service/impl/UserServiceImpl.java
@@ -3,7 +3,6 @@ package me.dio.service.impl;
 import me.dio.domain.model.User;
 import me.dio.domain.repository.UserRepository;
 import me.dio.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.NoSuchElementException;
